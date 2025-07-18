12a5c7d3e6178542bb9a88606b898e6ed14532cc
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-07-18 09:18:41 +0500

    ugly hex to dec converter

--- main.c
+++ main.c
@@ -1,53 +1,67 @@
 #include <stdio.h>
-#define MAXLEN 1000
+#include <math.h>
 
-void inputline(char s[]);
-int formats(char input[], char output[]);
+void gethexinput(char output[]);
+int htoi(char hx[]);
 
 int main(){
-    // char input[MAXLEN];
-    // char output[MAXLEN];
+    char hex[100];
 
-    // inputline(input);
-    // formats(input, output); // fuck
-    // // hwody bro
-    // printf("%s", output); //
-
-    printf("%d", -1L < 1UL);
-
-    return 0; // 111
-}
-
-void inputline(char s[]){
-    int c = getchar();
-    int i;
-    for (i = 0; c != EOF; ++i){
-        s[i] = c;
-        c = getchar();
-    }
-    s[i] = EOF;
-}
-
-int formats(char input[], char output[]){
-    int iscomment = 0;
-
-    if (input[0] == '\n') {
-        return 0;
+    for (int i = 0; i < 100; ++i) {
+        hex[i] = '0';
     }
 
-    for (int i = 0; input[i + 1] != EOF; ++i) {
-        if (input[i] == '/' && input[i+1] == '/') {
-            iscomment = 1;
-        }
-        if (input[i] == '\n'){
-            iscomment = 0;
-        }
+    gethexinput(hex);
 
-        if (iscomment) {
-            output[i] = ' ';
-        } else {
-            output[i] = input[i];
-        }
-    }
+    int dec = htoi(hex);
+
+    printf("%d\n", dec);
+
     return 0;
+}
+
+void gethexinput(char output[]){
+    char c;
+    int cnt = 0;
+    char interm[100];
+
+    for (int i = 0; i < 100; ++i) {
+        interm[i] = '0';
+    }
+
+    for (int i = 0; i < 100 && ((c = getchar()) != '\n'); ++i) {
+        interm[i] = c;
+        ++cnt;
+    }
+
+    for (int i = cnt; i > -1; --i) {
+        output[cnt - i] = interm[i - 1];
+    }
+
+    for (int i = cnt; i < 100; ++i) {
+        output[i] = '0';
+    }
+}
+
+int htoi(char hx[]){
+    char c;
+    long int dec = 0;
+    for (int i = 0; i < 100; ++i) {
+        c = hx[i];
+        char decchar;
+
+        if (c >= '0' && c <= '9') {
+            decchar = c - '0'; 
+        } else if (c >= 'A' && c <= 'Z') {
+            decchar = 10 + c - 'A';
+        } else if (c >= 'a' && c <= 'z') {
+            decchar = 10 + c - 'a';
+        } else {
+            printf("something went wrong, only 0-9, a-z, A-Z are supported");
+            return 0;
+        }
+
+        dec = dec + decchar * pow(16.0, i);
+    }
+    return dec;
 }
