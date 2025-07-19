cfa4e13d440ee234dc1a25abef6cc5db5072b08e
Artemy Medvedev <artemiy.logmy@gmail.com>
2025-07-19 16:11:53 +0500

    one if in while loop binary search

--- main.c
+++ main.c
@@ -1,67 +1,49 @@
 #include <stdio.h>
-#include <math.h>
 
-void gethexinput(char output[]);
-int htoi(char hx[]);
+int binary(int x, int v[], int n);
 
-int main(){
-    char hex[100];
+int main() {
+    int arraylen = 100;
+    int v[arraylen];
 
-    for (int i = 0; i < 100; ++i) {
-        hex[i] = '0';
+    for (int i = 0; i < arraylen; i++) {
+        v[i] = i*2;
     }
 
-    gethexinput(hex);
+    for (int i = 0; i < arraylen; i++) {
+        printf("%d\n", v[i]);
+    }
+    printf("\n");
 
-    int dec = htoi(hex);
+    int res = binary(20, v, arraylen);
 
-    printf("%d\n", dec);
+    printf("%d", res);
 
     return 0;
 }
 
-void gethexinput(char output[]){
-    char c;
-    int cnt = 0;
-    char interm[100];
+int binary(int x, int v[], int n) {
+    int low, high, mid;
 
-    for (int i = 0; i < 100; ++i) {
-        interm[i] = '0';
-    }
+    low = 0;
+    high = n - 1;
+    while (low < high) {
+        mid = (high + low) / 2;
 
-    for (int i = 0; i < 100 && ((c = getchar()) != '\n'); ++i) {
-        interm[i] = c;
-        ++cnt;
-    }
-
-    for (int i = cnt; i > -1; --i) {
-        output[cnt - i] = interm[i - 1];
-    }
-
-    for (int i = cnt; i < 100; ++i) {
-        output[i] = '0';
-    }
-}
-
-int htoi(char hx[]){
-    char c;
-    long int dec = 0;
-    for (int i = 0; i < 100; ++i) {
-        c = hx[i];
-        char decchar;
-
-        if (c >= '0' && c <= '9') {
-            decchar = c - '0'; 
-        } else if (c >= 'A' && c <= 'Z') {
-            decchar = 10 + c - 'A';
-        } else if (c >= 'a' && c <= 'z') {
-            decchar = 10 + c - 'a';
+        if (x < v[mid]) {
+            high = mid - 1;
         } else {
-            printf("something went wrong, only 0-9, a-z, A-Z are supported");
-            return 0;
+            low = mid + 1;
         }
-
-        dec = dec + decchar * pow(16.0, i);
     }
-    return dec;
+
+    if (v[high] == x) {
+        printf("a");
+        return high;
+    }
+    if (v[high - 1] == x) {
+        printf("b");
+        return high - 1;
+    }
+    return -1;
 }
